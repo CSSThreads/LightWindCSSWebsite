65fe4390215d45da55955530f1ccf3238862f69c
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-07-03 23:40:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ window.addEventListener('load', () => {
             }
         }
 
-        styleStr += `.${LightJsClasses[i].replace(/\:/g, '\\:').replace(/\[/g, '\\[').replace(/\]/g, '\\]')}`
+        styleStr += `.${LightJsClasses[i].replace(/\:/g, '\\:').replace(/\[/g, '\\[').replace(/\]/g, '\\]').replace(/\>/g, '\\>')}`
 
         for (let j = 0; j < classParams.length - 1; j++) {
             // selectors
@@ -59,21 +59,23 @@ window.addEventListener('load', () => {
         }
 
         styleStr += "{"
-        
-        /* ==========================================================================================
-        Proprieties defining statred
-        ========================================================================================== */ 
 
         let isFound = false;
         for (let k = 0; k < lightWindCssConfig.proprieties.light.length; k++) {
             if (isFound)
                 break;
-            if (classParams[classParams.length - 1].split('-')[0] == lightWindCssConfig.proprieties.light[k].name) {
-                let value = classParams[classParams.length - 1].split('-')[1]
-                for (let l = 2; l < classParams[classParams.length - 1].split('-').length; l++) {
-                    value += classParams[classParams.length - 1].split('-')[l]
-                }
+            if (classParams[classParams.length - 1].split('>')[0] == lightWindCssConfig.proprieties.light[k].name) {
+                let value = classParams[classParams.length - 1].split('>')[1]
                 value = value.substr(1, value.length - 2)
+
+                for (let l in lightWindCssConfig.proprieties.light[k].values) {
+                    if (lightWindCssConfig.proprieties.light[k].values[l].name == value)
+                        value = lightWindCssConfig.proprieties.light[k].values[l].value
+                }
+
+                if (value.startsWith('--')) {
+                    value = `var(${value})`
+                }
 
                 lightWindCssConfig.proprieties.light[k].proprieties.forEach(el => {
                     styleStr += `${el}: ${value};`
@@ -87,19 +89,26 @@ window.addEventListener('load', () => {
             if (isFound)
                 break;
             if (lightWindCssConfig.proprieties.global[k]["value-only"]) {
-                if (lightWindCssConfig.proprieties.global[k].values.indexOf(classParams[classParams.length - 1].split('-')[0]) != -1) {
-                    styleStr += `${lightWindCssConfig.proprieties.global[k].propriety}: ${classParams[classParams.length - 1].split('-')[0]}`
+                if (lightWindCssConfig.proprieties.global[k].values.indexOf(classParams[classParams.length - 1].split('>')[0]) != -1) {
+                    styleStr += `${lightWindCssConfig.proprieties.global[k].propriety}: ${classParams[classParams.length - 1].split('>')[0]}`
                     isFound = true;
                 }
             }
             else {
                 // with value to resolve
-                if (classParams[classParams.length - 1].split('-')[0] == lightWindCssConfig.proprieties.global[k].propriety || lightWindCssConfig.proprieties.global[k].alias.indexOf(classParams[classParams.length - 1].split('-')[0]) != -1) {
-                    let value = classParams[classParams.length - 1].split('-')[1]
-                    for (let l = 2; l < classParams[classParams.length - 1].split('-').length; l++) {
-                        value += classParams[classParams.length - 1].split('-')[l]
-                    }
+                if (classParams[classParams.length - 1].split('>')[0] == lightWindCssConfig.proprieties.global[k].propriety || lightWindCssConfig.proprieties.global[k].alias.indexOf(classParams[classParams.length - 1].split('>')[0]) != -1) {
+                    let value = classParams[classParams.length - 1].split('>')[1]
                     value = value.substr(1, value.length - 2)
+
+                    for (let l in lightWindCssConfig.proprieties.global[k].values) {
+                        if (lightWindCssConfig.proprieties.global[k].values[l].name == value)
+                            value = lightWindCssConfig.proprieties.global[k].values[l].value
+                    }
+
+                    if (value.startsWith('--')) {
+                        value = `var(${value})`
+                    }
+
                     styleStr += `${lightWindCssConfig.proprieties.global[k].propriety}: ${value}`
                     isFound = true;
                 }
@@ -107,21 +116,17 @@ window.addEventListener('load', () => {
         }
 
         if (!isFound) {
-            let value = classParams[classParams.length - 1].split('-')[1]
-            for (let l = 2; l < classParams[classParams.length - 1].split('-').length; l++) {
-                value += classParams[classParams.length - 1].split('-')[l]
-            }
+            let value = classParams[classParams.length - 1].split('>')[1]
             value = value.substr(1, value.length - 2)
 
-            styleStr += `${classParams[classParams.length - 1].split('-')[0]}: ${value}`;
+            if (value.startsWith('--')) {
+                value = `var(${value})`
+            }
+
+            styleStr += `${classParams[classParams.length - 1].split('>')[0]}: ${value}`;
         }
 
-        /* ==========================================================================================
-        Proprieties defining ended
-        ========================================================================================== */ 
-
         styleStr += ";}"
-
         if (breakPointOpen)
             styleStr += '}'
 
@@ -135,126 +140,16 @@ window.addEventListener('load', () => {
 // classes observation functions
 function lightObserve(classes) {
     new MutationObserver((mutations) => {
-        let styleLightWind = document.head.querySelector("style[data=\"lightwindcss\"]")
-
         for (let i in mutations) {
             // in case of the element creation
-            mutations[i].addedNodes.forEach(el => {
-                el.classList.forEach(cel => {
-                    if (classes.indexOf(cel) === -1) {
-                        classes.push(cel)
-                        let classParams = cel.split(':');
-                        for (let j in cel.split(':')) {
-                            let temp = cel.split(':')[j]
-                            classParams = temp.split('-')
-                        }
-                        console.log(classParams)
-                    }
+            mutations[i].addedNodes.forEach(cel => {
+                cel.classList.forEach(el => {
+                    classEvaluate(el, classes)
                 })
             })
             // in case of the element class addition
             mutations[i].target.classList.forEach(el => {
-                if (classes.indexOf(el) === -1) {
-                    classes.push(el)
-
-                    // ========================================================================================================
-                    // ========================================================================================================
-
-                    let classParams = el.split(':');
-
-                    let styleStr = ``;
-                    let breakPointOpen = false;
-
-                    // getting the breakpoint
-
-                    for (let k in lightWindCssConfig.breakpoints.screens) {
-                        if (lightWindCssConfig.breakpoints.screens[k].name == classParams[0]) {
-                            if (lightWindCssConfig.breakpoints.screens[k].min == null) {
-                                styleStr += `@media screen and (max-width: ${lightWindCssConfig.breakpoints.screens[k].max}) { `;
-                                breakPointOpen = true;
-                            }
-                            else if (lightWindCssConfig.breakpoints.screens[k].max == null) {
-                                styleStr += `@media screen and (min-width: ${lightWindCssConfig.breakpoints.screens[k].min}) { `;
-                                breakPointOpen = true;
-                            }
-                            else {
-                                styleStr += `@media screen and (min-width: ${lightWindCssConfig.breakpoints.screens[k].min}) and (max-width: ${lightWindCssConfig.breakpoints.screens[k].max}) {`;
-                                breakPointOpen = true;
-                            }
-                            break;
-                        }
-                    }
-
-                    styleStr += `.${el.replace(/\:/g, '\\:').replace(/\[/g, '\\[').replace(/\]/g, '\\]')}`
-
-                    for (let j = 0; j < classParams.length - 1; j++) {
-                        // selectors
-                        if (breakPointOpen && j == 0)
-                            j++;
-                        
-                        for (let k = 0; k < lightWindCssConfig.selectors[':'].length; k++) {
-                            if (lightWindCssConfig.selectors[':'][k].name == classParams[j] || lightWindCssConfig.selectors[':'][k].alias.indexOf(classParams[j]) != -1) {
-                                styleStr += `:${lightWindCssConfig.selectors[':'][k].selector}`
-                            }
-                        }
-                        for (let k = 0; k < lightWindCssConfig.selectors['::'].length; k++) {
-                            if (lightWindCssConfig.selectors['::'][k].name == classParams[j] || lightWindCssConfig.selectors['::'][k].alias.indexOf(classParams[j]) != -1) {
-                                styleStr += `::${lightWindCssConfig.selectors['::'][k].selector}`
-                            }
-                        }
-                    }
-
-                    styleStr += "{"
-                    
-                    /* ==========================================================================================
-                    Proprieties defining statred
-                    ========================================================================================== */ 
-
-                    let isFound = false;
-                    for (let k = 0; k < lightWindCssConfig.proprieties.global.length; k++) {
-                        if (isFound)
-                            break;
-                        if (lightWindCssConfig.proprieties.global[k]["value-only"]) {
-                            if (lightWindCssConfig.proprieties.global[k].values.indexOf(classParams[classParams.length - 1].split('-')[0]) != -1) {
-                                styleStr += `${lightWindCssConfig.proprieties.global[k].propriety}: ${classParams[classParams.length - 1].split('-')[0]}`
-                                isFound = true;
-                            }
-                        }
-                        else {
-                            // with value to resolve
-                            if (classParams[classParams.length - 1].split('-')[0] == lightWindCssConfig.proprieties.global[k].propriety || lightWindCssConfig.proprieties.global[k].alias.indexOf(classParams[classParams.length - 1].split('-')[0]) != -1) {
-                                let value = classParams[classParams.length - 1].split('-')[1]
-                                for (let l = 2; l < classParams[classParams.length - 1].split('-').length; l++) {
-                                    value += classParams[classParams.length - 1].split('-')[l]
-                                }
-                                value = value.substr(1, value.length - 2)
-                                styleStr += `${lightWindCssConfig.proprieties.global[k].propriety}: ${value}`
-                                isFound = true;
-                            }
-                        }
-                    }
-
-                    if (!isFound) {
-                        let value = classParams[classParams.length - 1].split('-')[1]
-                        for (let l = 2; l < classParams[classParams.length - 1].split('-').length; l++) {
-                            value += classParams[classParams.length - 1].split('-')[l]
-                        }
-                        value = value.substr(1, value.length - 2)
-
-                        styleStr += `${classParams[classParams.length - 1].split('-')[0]}: ${value}`;
-                    }
-
-                    /* ==========================================================================================
-                    Proprieties defining ended
-                    ========================================================================================== */ 
-
-                    styleStr += ";}"
-
-                    if (breakPointOpen)
-                        styleStr += '}'
-
-                    styleLightWind.innerText += styleStr
-                }
+                classEvaluate(el, classes)
             })
         }
     }).observe(document.querySelector('body'), { attributes: true, subtree: true, childList: true });
@@ -269,4 +164,129 @@ function getAllClasses() {
         })
     }
     return allClasses;
+}
+function classEvaluate(el, classes = null) {
+    let styleLightWind = document.head.querySelector("style[data=\"lightwindcss\"]")
+
+    if (classes.indexOf(el) === -1) {
+        classes.push(el)
+
+        let classParams = el.split(':');
+
+        let styleStr = ``;
+        let breakPointOpen = false;
+
+        // getting the breakpoint
+
+        for (let k in lightWindCssConfig.breakpoints.screens) {
+            if (lightWindCssConfig.breakpoints.screens[k].name == classParams[0]) {
+                if (lightWindCssConfig.breakpoints.screens[k].min == null) {
+                    styleStr += `@media screen and (max-width: ${lightWindCssConfig.breakpoints.screens[k].max}) { `;
+                    breakPointOpen = true;
+                }
+                else if (lightWindCssConfig.breakpoints.screens[k].max == null) {
+                    styleStr += `@media screen and (min-width: ${lightWindCssConfig.breakpoints.screens[k].min}) { `;
+                    breakPointOpen = true;
+                }
+                else {
+                    styleStr += `@media screen and (min-width: ${lightWindCssConfig.breakpoints.screens[k].min}) and (max-width: ${lightWindCssConfig.breakpoints.screens[k].max}) {`;
+                    breakPointOpen = true;
+                }
+                break;
+            }
+        }
+
+        styleStr += `.${el.replace(/\:/g, '\\:').replace(/\[/g, '\\[').replace(/\]/g, '\\]').replace(/\>/g, '\\>')}`
+
+        for (let j = 0; j < classParams.length - 1; j++) {
+            // selectors
+            if (breakPointOpen && j == 0)
+                j++;
+            
+            for (let k = 0; k < lightWindCssConfig.selectors[':'].length; k++) {
+                if (lightWindCssConfig.selectors[':'][k].name == classParams[j] || lightWindCssConfig.selectors[':'][k].alias.indexOf(classParams[j]) != -1) {
+                    styleStr += `:${lightWindCssConfig.selectors[':'][k].selector}`
+                }
+            }
+            for (let k = 0; k < lightWindCssConfig.selectors['::'].length; k++) {
+                if (lightWindCssConfig.selectors['::'][k].name == classParams[j] || lightWindCssConfig.selectors['::'][k].alias.indexOf(classParams[j]) != -1) {
+                    styleStr += `::${lightWindCssConfig.selectors['::'][k].selector}`
+                }
+            }
+        }
+
+        styleStr += "{"
+
+        let isFound = false;
+        for (let k = 0; k < lightWindCssConfig.proprieties.light.length; k++) {
+            if (isFound)
+                break;
+            if (classParams[classParams.length - 1].split('>')[0] == lightWindCssConfig.proprieties.light[k].name) {
+                let value = classParams[classParams.length - 1].split('>')[1]
+                value = value.substr(1, value.length - 2)
+
+                for (let l in lightWindCssConfig.proprieties.light[k].values) {
+                    if (lightWindCssConfig.proprieties.light[k].values[l].name == value)
+                        value = lightWindCssConfig.proprieties.light[k].values[l].value
+                }
+
+                if (value.startsWith('--')) {
+                    value = `var(${value})`
+                }
+
+                lightWindCssConfig.proprieties.light[k].proprieties.forEach(el => {
+                    styleStr += `${el}: ${value};`
+                })
+
+                isFound = true;
+            }
+        }
+
+        for (let k = 0; k < lightWindCssConfig.proprieties.global.length; k++) {
+            if (isFound)
+                break;
+            if (lightWindCssConfig.proprieties.global[k]["value-only"]) {
+                if (lightWindCssConfig.proprieties.global[k].values.indexOf(classParams[classParams.length - 1].split('>')[0]) != -1) {
+                    styleStr += `${lightWindCssConfig.proprieties.global[k].propriety}: ${classParams[classParams.length - 1].split('>')[0]}`
+                    isFound = true;
+                }
+            }
+            else {
+                // with value to resolve
+                if (classParams[classParams.length - 1].split('>')[0] == lightWindCssConfig.proprieties.global[k].propriety || lightWindCssConfig.proprieties.global[k].alias.indexOf(classParams[classParams.length - 1].split('>')[0]) != -1) {
+                    let value = classParams[classParams.length - 1].split('>')[1]
+                    value = value.substr(1, value.length - 2)
+
+                    for (let l in lightWindCssConfig.proprieties.global[k].values) {
+                        if (lightWindCssConfig.proprieties.global[k].values[l].name == value)
+                            value = lightWindCssConfig.proprieties.global[k].values[l].value
+                    }
+
+                    if (value.startsWith('--')) {
+                        value = `var(${value})`
+                    }
+
+                    styleStr += `${lightWindCssConfig.proprieties.global[k].propriety}: ${value}`
+                    isFound = true;
+                }
+            }
+        }
+
+        if (!isFound) {
+            let value = classParams[classParams.length - 1].split('>')[1]
+            value = value.substr(1, value.length - 2)
+
+            if (value.startsWith('--')) {
+                value = `var(${value})`
+            }
+
+            styleStr += `${classParams[classParams.length - 1].split('>')[0]}: ${value}`;
+        }
+
+        styleStr += ";}"
+        if (breakPointOpen)
+            styleStr += '}'
+
+        styleLightWind.innerText += styleStr
+    }
 }
