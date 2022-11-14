36ec8c8bc88e079ca0595eb1cb53845737ea86e0
Jasiukiewicz Tymon <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-11-14 17:44:36 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,70 @@
 <script>
+  import { onMount } from 'svelte';
+
   let scrollTop = 0;
   let top = -80;
-  let content = print('<span class="color>red">Write your HTML here!</span>')
-  let lines = [content]
+  let content = ''
+  let lines = content.split('\n')
 
-  function print(line) {
-    let l = line, newl = [], tag = false;
-    
-    for (let args of l.split(' ')) {
-      let tnewl = []
-      args = args.replaceAll('<', ' &#60;').replaceAll('>', '> ')
-      for (let arg of args.split(' ')) {
-        if (arg.startsWith('&#60;/')) {
-          if (arg.endsWith('>')) {
-            tnewl.push()
+  function Update() {
+    var lines = content.split('\n')
+    document.querySelector('#template').innerHTML = ''
+    for (let i = 0; i < lines.length; i++) {
+      let l = lines[i].split(' '), newl = [], tag = false
+      for (let args of l) {
+        args = args.replaceAll('<', '&#60;')
+        for (let arg of args.split(' ')) {
+          if (arg.startsWith('&#60;/')) {
+            if (arg.endsWith('>')) {
+              newl.push(`&#60;/<span class="color>#fa8e8e">${arg.slice(6, arg.length - 1)}</span>>`)
+            }
+            else {
+              newl.push(`&#60;/<span class="color>#fa8e8e">${arg.slice(6, arg.length)}</span>`)
+              tag = true
+            }
+          }
+          else if (arg.startsWith('&#60;')) {
+            if (arg.endsWith('>')) {
+              newl.push(`&#60;<span class="color>#fa8e8e">${arg.slice(5, arg.length - 1)}</span>>`)
+            }
+            else {
+              newl.push(`&#60;<span class="color>#fa8e8e">${arg.slice(5, arg.length)}</span>`)
+              tag = true
+            }
+          }
+          else if (tag) {
+            if (!arg.includes('=')) {
+              if (arg.endsWith('>')) {
+                newl.push(`<span class="color>#fac68e">${arg.slice(0, arg.length - 1)}</span>>`)
+                tag = false
+              }
+              else {
+                newl.push(`<span class="color>#fac68e">${arg}</span>`)
+              }
+            }
+            else {
+              if (arg.endsWith('>')) {
+                newl.push(`<span class="color>#fac68e">${arg.split('=')[0]}</span>=<span class="color>#93fa8e">${arg.split('=')[1].slice(0, arg.split('=')[1].length - 1)}</span>>`)
+                tag = false
+              }
+              else {
+                newl.push(`<span class="color>#fac68e">${arg.split('=')[0]}</span>=<span class="color>#93fa8e">${arg.split('=')[1]}</span>`)
+              }
+            }
+          }
+          else {
+            newl.push(arg)
           }
         }
       }
-      newl.push(tnewl)
+      document.querySelector('#template').innerHTML += `<div class="min-height>38px mb>-19px w>100% relative grid grid-template-columns>50px_1fr"><span class="counter">${i + 1}</span><div class="w>calc(100%_-_20px)">
+        <pre class="align>right" style="max-width: ${document.querySelector('textarea').offsetWidth - 51}px">
+${newl.join(' ')}
+        </pre>
+      </div></div>`
     }
-
-    //print
-    //l.replaceAll(' ', '<span class="white-space>pre"> </span>')
-    return l;
   }
   function scrollPage() {
-    //scrollTop = window.innerHeight * (document.getElementById('container').scrollTop / (document.querySelector('main').offsetHeight + document.querySelector('header').offsetHeight - window.innerHeight)) + 80;
 
     scrollTop = document.getElementById('container').scrollTop / (document.querySelector('main').offsetHeight + document.querySelector('header').offsetHeight - window.innerHeight) * 100
 
@@ -34,6 +73,25 @@
   function copyText() {
     navigator.clipboard.writeText(document.getElementById('include').innerText);
   }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    Update();
+  })
+
+  async function UpdateHeight(e) {
+    document.querySelector('textarea').style.height = "1px"; 
+    document.querySelector('textarea').style.height = (25+document.querySelector('textarea').scrollHeight)+"px";
+  }
+  async function UpdateSize() {
+    for (let el of document.querySelectorAll('pre')) {
+      el.style.maxWidth = `${document.querySelector('textarea').offsetWidth - 51}px`
+    }
+    UpdateHeight();
+  }
+
+  onMount(() => {
+ 		window.addEventListener('resize', UpdateSize);
+ 	});
 </script>
 <div id="container" on:scroll={scrollPage} on:loadeddata={scrollPage}>
   <div class="w>calc(100%_-_50px) sm:w>calc(100%_-_250px) max-width>1000px absolute l>50% transform>translateX(-50%) h>100% pl>50px sm:pr>0px pr>50px">
@@ -56,12 +114,9 @@
       <h2 id="h2-Playground" class="cursor>pointer"><a href="{window.location.href.split('#')[0]}#h2-Playground">Playground</a></h2>
       <p>Test LightWindCSS here! Change the classes with the next syntax: <b><i>(screen-selector:)(selector:)</i>&#60;value-only|key>value></b></p>
       <div id="demoBox" class="counter-reset>i grid gap>20px grid-template-columns>1fr_1fr p>20px background>#422271 w>calc(100%_-_65px) h>700px border-radius>20px">
-        <div class="overflow>hidden relative w>100%">
-          <textarea id="editor" class="pl>51px white-space>normal resize>none absolute z-index>10 w>calc(100%_-_65px) h>calc(100%_-_10px) color>#000 opacity>0.3 text-align>left" bind:value={content} on:input={() => lines = content.split('\n')}></textarea>
-          <div class="absolute mt>3px ml>3px w>100%">
-            {#each lines as line, i}
-              <div class="w>calc(100%_-_15px) grid grid-template-columns>50px_1fr"><span class="text-align>right pr>10px w>40px display>inline-block opacity>0.5">{i + 1}</span><div class="w>100%">{@html print(line)}</div></div>
-            {/each}
+        <div class="overflow>auto relative w>100%">
+          <textarea spellcheck="false" id="editor" class="pl>51px white-space>normal resize>none absolute z-index>10 w>calc(100%_-_65px) resize>none opacity>1 color>transparent background>transparent outline>none border>none caret-color>white text-align>left" bind:value={content} on:input={() => { Update() }} on:keydown={UpdateHeight}></textarea>
+          <div id="template" class="absolute mt>3px white-space>normal ml>3px w>calc(100%_-_15px)">
           </div>
         </div>
         <div id="demoHtmlResult" class="relative overflow>auto">
